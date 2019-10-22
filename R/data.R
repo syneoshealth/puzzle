36415055ce59d681421e3264d93049aea3feab0b
syneoshealth <mario.gonzalezsales@syneoshealth.com>
2019-10-22 14:21:24 +0200

    data description

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' Covariate values.
+#'
+#' A dataset containing covariate information.
+#'
+#' @format A tibble with 12 rows and 4 variables:
+#' \describe{
+#'   \item{ID}{Individual}
+#'   \item{TIME}{Time, in hours}
+#'   \item{VARIABLE}{Variable}
+#'   \item{VALUE}{Value of the variable}
+#' }
+"df_cov"
+
