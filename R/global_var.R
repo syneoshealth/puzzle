b3b7e5a27df6f03ea0059970409eb1e7433a087d
syneoshealth <mario.gonzalezsales@syneoshealth.com>
2019-10-25 10:15:15 +0200

    puzzle() fix

--- R/global_var.R
+++ R/global_var.R
@@ -1,2 +1,14 @@
 # Make global variables
 globalVariables(c("DATETIME", "ID", "TIME","DATASOURCE", "BLQ0", "DV","BLQ1", "BLQLLOQ", "DOSETIME", "SORTINDEX", "VARIABLE"), add = FALSE)
+
+file.ext = function(x) {
+  ext=regmatches(x, regexec("\\.([^\\.]+$)",x))[[1]][2]
+  if (is.na(ext)) ext=""
+  return(ext)
+}
+
+file.name = function(x) {
+  name=regmatches(x, regexec("(.*)\\.[^\\.]+$",x))[[1]][2]
+  if (is.na(name)) name=x
+  return(name)
+}
--- R/puzzle.R
+++ R/puzzle.R
@@ -180,17 +180,6 @@ puzzle = function(directory=NULL,
     close(fileConn)
   }
 
-  file.ext = function(x) {
-    ext=regmatches(x, regexec("\\.([^\\.]+$)",x))[[1]][2]
-    if (is.na(ext)) ext=""
-    return(ext)
-  }
-
-  file.name = function(x) {
-    name=regmatches(x, regexec("(.*)\\.[^\\.]+$",x))[[1]][2]
-    if (is.na(name)) name=x
-    return(name)
-  }
   
   if (is.null(directory)) directory="" 
 
