2b9fef6a4548a71ade2939f5be2f8aca8b55ac57
syneoshealth <mario.gonzalezsales@syneoshealth.com>
2019-10-31 23:35:52 +0100

    test

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(puzzle)
+
+test_check("puzzle")
---
+++ tests/testthat/test_puzzle.R
@@ -0,0 +1,15 @@
+context("Test puzzle functionality")
+test_that("Check if puzzle was properly installed",{
+  nm = list(pk = list(parent=as.data.frame(puzzle::df_pk_start)),
+            dose=as.data.frame(puzzle::df_dose_start),
+            cov=as.data.frame(puzzle::df_cov_start))
+ df = puzzle(directory=file.path(getwd()),
+         order=c(0),
+         pk=list(data=nm$pk), 
+         dose=list(data=nm$dose), 
+         cov=list(data=nm$cov)) 
+expect_equal(ncol(df),15)  
+})
+
+
+devtools::test()
