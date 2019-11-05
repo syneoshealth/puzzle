dcccec3ec1856e5e44b7b9cd8af4c2059cc608db
syneoshealth <mario.gonzalezsales@syneoshealth.com>
2019-11-05 19:47:55 +0100

    test_puzzle

--- tests/testthat/test_puzzle.R
+++ tests/testthat/test_puzzle.R
@@ -4,12 +4,12 @@ test_that("Check if puzzle was properly installed",{
   nm = list(pk = list(parent=as.data.frame(puzzle::df_pk_start)),
             dose=as.data.frame(puzzle::df_dose_start),
             cov=as.data.frame(puzzle::df_cov_start))
- df = puzzle(directory=file.path(getwd()),
+ df_puzzle_test = puzzle(directory=file.path(getwd()),
          order=c(0),
          pk=list(data=nm$pk), 
          dose=list(data=nm$dose), 
          cov=list(data=nm$cov)) 
-expect_equal(class(df),"data.frame")  
+ expect_output(str(df_puzzle_test), "16 obs")
 })
 
 
