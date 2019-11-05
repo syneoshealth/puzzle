5d63ad5bea981e7f20986f4a3e3c6c69abfb39c3
syneoshealth <mario.gonzalezsales@syneoshealth.com>
2019-11-05 19:54:23 +0100

    test

--- tests/testthat/test_puzzle.R
+++ tests/testthat/test_puzzle.R
@@ -1,14 +1,14 @@
 library(testthat)
 context("Test puzzle functionality")
+nm = list(pk = list(parent=as.data.frame(puzzle::df_pk_start)),
+          dose=as.data.frame(puzzle::df_dose_start),
+          cov=as.data.frame(puzzle::df_cov_start))
+df_puzzle_test = puzzle(directory=file.path(getwd()),
+                        order=c(0),
+                        pk=list(data=nm$pk), 
+                        dose=list(data=nm$dose), 
+                        cov=list(data=nm$cov)) 
 test_that("Check if puzzle was properly installed",{
-  nm = list(pk = list(parent=as.data.frame(puzzle::df_pk_start)),
-            dose=as.data.frame(puzzle::df_dose_start),
-            cov=as.data.frame(puzzle::df_cov_start))
- df_puzzle_test = puzzle(directory=file.path(getwd()),
-         order=c(0),
-         pk=list(data=nm$pk), 
-         dose=list(data=nm$dose), 
-         cov=list(data=nm$cov)) 
  expect_output(str(df_puzzle_test), "16 obs")
 })
 
