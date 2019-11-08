e42ba85f6c891d50ea490beeb45dd16c784473a7
syneoshealth <mario.gonzalezsales@syneoshealth.com>
2019-11-08 10:17:08 +0100

    CRAN comments

--- R/puzzle.R
+++ R/puzzle.R
@@ -30,8 +30,15 @@
 #' @param username define person performing the assembling
 #' 
 #' @return a pharmacometrics ready data set
-#' @examples nm = list(pk = list(parent=as.data.frame(puzzle::df_pk_start)),dose=as.data.frame(puzzle::df_dose_start), cov=as.data.frame(puzzle::df_cov_start))
-#' puzzle::puzzle(directory=file.path(getwd()), order=c(0), pk=list(data=nm$pk), dose=list(data=nm$dose), cov=list(data=nm$cov))
+#' @examples 
+#' nm = list(pk = list(parent=as.data.frame(puzzle::df_pk_start)),
+#'           dose=as.data.frame(puzzle::df_dose_start), 
+#'           cov=as.data.frame(puzzle::df_cov_start))
+#' puzzle(directory=file.path(getwd()), 
+#'        order=c(0), 
+#'        pk=list(data=nm$pk), 
+#'        dose=list(data=nm$dose), 
+#'        cov=list(data=nm$cov))
 #' 
 #' @export
 #' 
